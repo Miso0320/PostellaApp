0e5a3a5fbbc73dd876e77933752ca320887eea60
yuyu062 <chorong6544@naver.com>
2023-09-06 09:02:17 +0900

    nav-graph추가 및 화면 이동 기능 추가

--- app/src/main/java/com/appteam4/postella/ui/AdListFragment.java
+++ app/src/main/java/com/appteam4/postella/ui/AdListFragment.java
@@ -3,64 +3,44 @@ package com.appteam4.postella.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.appteam4.postella.R;
+import com.appteam4.postella.databinding.FragmentAdListBinding;
+import com.appteam4.postella.databinding.FragmentMyOrderListBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AdListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AdListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ADListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdListFragment newInstance(String param1, String param2) {
-        AdListFragment fragment = new AdListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "AdListFragment";
+    private FragmentAdListBinding binding;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ad_list, container, false);
+        binding = FragmentAdListBinding.inflate(inflater);
+        navController = NavHostFragment.findNavController(this);
+
+        initBtnGoBack();
+        initBtnAdDetail();
+
+        return binding.getRoot();
+    }
+
+    private void initBtnGoBack(){
+        binding.btnGoBack.setOnClickListener(v->{
+            navController.popBackStack();
+        });
+    }
+
+    private  void initBtnAdDetail(){
+        binding.btnAdDetail.setOnClickListener(v->{
+            navController.navigate(R.id.action_dest_ad_list_to_dest_ad_detail);
+        });
     }
 }
--- app/src/main/java/com/appteam4/postella/ui/MyPageOrderListFragment.java
+++ app/src/main/java/com/appteam4/postella/ui/MyPageOrderListFragment.java
@@ -3,64 +3,42 @@ package com.appteam4.postella.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.appteam4.postella.R;
+import com.appteam4.postella.databinding.FragmentMyOrderListBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyPageOrderListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyPageOrderListFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyPageOrderListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyOrderListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyPageOrderListFragment newInstance(String param1, String param2) {
-        MyPageOrderListFragment fragment = new MyPageOrderListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "MyPageOrderListFragment";
+    private FragmentMyOrderListBinding binding;
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_order_list, container, false);
+       binding = FragmentMyOrderListBinding.inflate(inflater);
+       navController = NavHostFragment.findNavController(this);
+
+       initBtnGoBack();
+       initBtnProdDtail();
+
+       return binding.getRoot();
+    }
+
+    private void initBtnGoBack(){
+        binding.btnGoBack.setOnClickListener(v->{
+            navController.popBackStack();
+        });
+    }
+
+    private void initBtnProdDtail(){
+        binding.btnProdDetail.setOnClickListener(v->{
+            navController.navigate(R.id.action_dest_mypage_order_list_to_dest_prod_detail);
+        });
     }
 }
