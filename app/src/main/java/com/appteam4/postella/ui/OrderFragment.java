a132d1c2fa505510400b24d7c13524ecd2e574d8
Miso0320 <[miso8493@hanmail.net]>
2023-09-06 09:00:07 +0900

    버튼 및 프래그먼트 이동 코드 추가

--- app/src/main/java/com/appteam4/postella/ui/OrderFragment.java
+++ app/src/main/java/com/appteam4/postella/ui/OrderFragment.java
@@ -3,64 +3,37 @@ package com.appteam4.postella.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.appteam4.postella.R;
+import com.appteam4.postella.databinding.FragmentLoginBinding;
+import com.appteam4.postella.databinding.FragmentOrderBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class OrderFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OrderFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OrderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OrderFragment newInstance(String param1, String param2) {
-        OrderFragment fragment = new OrderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "OrderFragment";
+    private FragmentOrderBinding binding;
+    NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        binding = FragmentOrderBinding.inflate(inflater);
+        navController = NavHostFragment.findNavController(this);
+
+        initBtnMain();
+
+        return binding.getRoot();
+    }
+
+    private void initBtnMain() {
+        binding.btnMyPage.setOnClickListener(v -> {
+            navController.navigate(R.id.action_dest_order_to_dest_mypage_order_list);
+            //navController.popBackStack(R.id.dest_main,false);
+        });
     }
 }
